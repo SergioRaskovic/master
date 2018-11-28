6c3418fb15bf1a79d98b61ca16b2de123dbbd313
SergioRaskovic <45363238+SergioRaskovic@users.noreply.github.com>
2018-11-28 20:56:58 +0100

    jos uvijek ucimo

---
+++ ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,31 @@
+﻿// ConsoleApplication5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int M;
+	int N;
+	int R;
+	cin >> M >> N >> R;
+	R = N - M;
+	cout << N - M;
+
+		return 0;
+}
+
+//Markov tata obožava brzo voziti, što Marka jako ljuti jer se boji policije.Stoga Marko tijekom vožnje stalno prati znakove ograničenja brzine pokraj ceste i brzinu kojom vozi njegov otac, te ocu redovito govori koliko iznad dozvoljene brzine vozi.Pomalo se već umorio od računanja razlika brzina te moli tebe za pomoć.Tvoj zadatak je napisati program koji će na osnovu maksimalne dozvoljene brzine i brzine kojom se kreće Markov otac izračunati za koliko je Markov otac premašio maksimalnu dozvoljenu brzinu.
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
