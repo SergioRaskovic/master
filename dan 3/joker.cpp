169fe71f8928c05d856259edc33f6d26bfbd7b21
SergioRaskovic <45363238+SergioRaskovic@users.noreply.github.com>
2018-12-05 22:55:22 +0100

    kl

--- dan 3/joker.cpp
+++ dan 3/joker.cpp
@@ -18,13 +18,17 @@ int main()
 
 	for (int i = 0; i < 6; i++) {
 		cin >> a;
-		b = a % 10;;
+		b = a % 10;
 		joker[i] = b;
 	}
 	for (int j = 0; j < 3; j++) {
 		cin >> brojjokera[j];
 	}
-
+	for (int l = 0; l < 6; l++) {
+		if (joker[l] == sjebanbo[l]) {
+			d++
+		}
+	}
 	for (int k = 0; k < 3; k++) {
 		c = brojjokera[k];
 
@@ -35,30 +39,28 @@ int main()
 		sjebanbo[4] = (c - sjebanbo[0] * 100000 - sjebanbo[1] * 10000 - sjebanbo[2] * 1000 - sjebanbo[3] * 100) / 10;
 		sjebanbo[5] = (c - sjebanbo[0] * 100000 - sjebanbo[1] * 10000 - sjebanbo[2] * 1000 - sjebanbo[3] * 100 - sjebanbo[4] * 10);
 
-		for (int l = 5; l >= 0; l -= 1) {
-			if (joker[l] == sjebanbo[l]) {
-				d++;
 		
-		if (d < 2) 
-			cout << "Nedobitan" << endl;
+		
+				if (d < 2)
+					cout << "Nedobitan" << endl;
 
-		else if (d == 2)
-			cout << "V. vrsta" << endl;
+				else if (d == 2)
+					cout << "V. vrsta" << endl;
 
-		else if (d == 3)
-			cout << "IV. vrsta" << endl;
+				else if (d == 3)
+					cout << "IV. vrsta" << endl;
 
-		else if (d == 4)
-			cout << "III. vrsta" << endl;
+				else if (d == 4)
+					cout << "III. vrsta" << endl;
 
-		else if (d == 5)
-			cout << "II. vrsta" << endl;
+				else if (d == 5)
+					cout << "II. vrsta" << endl;
 
-		else if (d == 6)
-			cout << "I. vrsta" << endl;
+				else if (d == 6)
+					cout << "I. vrsta" << endl;
 
-			}
-		}
+			
+		
 	
 		d = 0;
 	}
